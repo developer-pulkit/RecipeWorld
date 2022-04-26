c1c0b0c44db2c5caa439b6b7606a061fd0a5882b
Pulkit <297pulkitmehta@gmail.com>
2022-04-26 01:11:28 -0400

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import React from "react";
+import { motion } from "framer-motion";
+
+function Home() {
+  return (
+    <motion.div>
+      <Veggie />
+      <Popular />
+    </motion.div>
+  );
+}
+
+export default Home;
